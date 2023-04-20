cec181f1feaeebc9593609da3fe12f47175f76f8
bs-ramesh <114816598+bs-ramesh@users.noreply.github.com>
2023-04-20 19:45:05 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en" >
+<head>
+  <meta charset="UTF-8">
+  <title>Examinee Login</title>
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+  <link rel="icon" type="login-ui/image/png" href="images/icons/favicon.ico"/>
+  <link rel="stylesheet" type="text/css" href="login-ui/vendor/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" type="text/css" href="login-ui/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" type="text/css" href="login-ui/fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+  <link rel="stylesheet" type="text/css" href="login-ui/vendor/animate/animate.css">
+  <link rel="stylesheet" type="text/css" href="login-ui/vendor/css-hamburgers/hamburgers.min.css">
+  <link rel="stylesheet" type="text/css" href="login-ui/vendor/select2/select2.min.css">
+  <link rel="stylesheet" type="text/css" href="login-ui/vendor/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" type="text/css" href="login-ui/css/util.css">
+  <link rel="stylesheet" type="text/css" href="login-ui/css/main.css">
+  <link rel="stylesheet" href="style.css">
+
+
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous"><link rel="stylesheet" href="./style.css">
+
+</head>
+<body>
+<!-- partial:index.partial.html -->
+<div class="box-form">
+	<div class="left">
+		<div class="overlay">
+	
+		<p style="opacity: 0;">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+		Curabitur et est sed felis aliquet sollicitudin</p>
+		
+		</div>
+	</div>
+	
+	
+		<div class="right"style="
+    padding-top: 80px;
+    padding-left: 40px;">
+
+		<!-- <h5>JSSTU</h5> -->
+		<img src="d1.png" alt="JSSTU" width="250 px" style="margin: 70 px ;">
+<!-- <p style="opacity: 0 ; height: 250 px;"> dijadjad9adjadajidjajdinabdiwjbadj</p> -->
+<form method="post" id="examineeLoginFrm" class="validate-form" style=" width: 100%;
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -moz-box;
+    display: -ms-flexbox;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;">		
+<div class="inputs">
+			
+			<br>
+			<input class="input100" type="text" name="username" placeholder="USN">
+			<input class="input100" type="password" name="pass" placeholder="Password">
+		</div>
+			
+			<br><br>
+			
+	
+			
+			<br><br>
+			<button type="submit" class="login100-form-btn" style="margin: 20px auto; border-radius:20px;">
+				Login
+			</button>
+</form>
+		</div>
+	
+</div>
+<!-- partial -->
+<script src="login-ui/vendor/jquery/jquery-3.2.1.min.js"></script>
+<script src="login-ui/vendor/animsition/js/animsition.min.js"></script>
+<script src="login-ui/vendor/bootstrap/js/popper.js"></script>
+<script src="login-ui/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="login-ui/vendor/select2/select2.min.js"></script>
+<script src="login-ui/vendor/daterangepicker/moment.min.js"></script>
+<script src="login-ui/vendor/daterangepicker/daterangepicker.js"></script>
+<script src="login-ui/vendor/countdowntime/countdowntime.js"></script>
+<script src="login-ui/js/main.js"></script>
+
+</body>
+</html>
